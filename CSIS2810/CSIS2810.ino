0e5af81607d4042cf2b2aeca49ebb04c03a75922
Edward Jenco <lukitree@gmail.com>
2017-07-12 10:09:22 -0600

    Add project files.

---
+++ CSIS2810/CSIS2810.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CSIS2810.ino
+ Created:	7/12/2017 10:09:15 AM
+ Author:	lukit
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
